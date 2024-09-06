f33adbc85d00d8bf962c9340433075a4c7435aba
Grandee Lee <grandee.lee@gmail.com>
2024-09-06 10:04:35 +0800

    daily

--- our-web-app/app.py
+++ our-web-app/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 
 app = Flask(__name__)  #__name__ = __main__
 
+app.secret_key = "2g3hjkrfhjk34hj"  # necessary for the session cookie
+
 @app.route('/')
+@app.route('/index')
+@app.route('/homepage')
 def home():
     return render_template('login.html', my_title="whatever")
 
@@ -11,14 +15,20 @@ def greet(user_input):
     result = '<p>This is {}.<\p>'.format(user_input)
     return render_template('base.html', display_string=result)
 
-@app.route("/greetWithArgs")
-def greetWithArgs():
-    name = request.args.get('friend').title()
-    return redirect(url_for('greet', user_input=name))
+@app.route('/cards')
+def cards():
+    return render_template('cards.html')
 
-@app.route("/submit_form", methods=['POST'])
+@app.route("/login", methods=['POST', 'GET'])
 def form():
     if request.method == 'POST':
         email = request.form.get('email')
         pswd = request.form.get('pswd')
-        return render_template('base.html', email=email, pswd=pswd)
+        if email != "admin@abc.com" or pswd != "12345":
+            flash("invalid credentials. please try again")
+            return render_template('login.html')
+        else:
+            flash("you were successfully loggin in")
+            return redirect(url_for('cards'))
+    if request.method == 'GET':
+        return render_template('login.html')
