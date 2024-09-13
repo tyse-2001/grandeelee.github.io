1efef2210c599a67b2f4a58fa447469750419a9b
Grandee Lee <grandee.lee@gmail.com>
2024-09-13 09:31:19 +0800

    update

--- our-web-app/app.py
+++ our-web-app/app.py
@@ -1,8 +1,30 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
+import csv
 
 app = Flask(__name__)  #__name__ = __main__
 
 app.secret_key = "2g3hjkrfhjk34hj"  # necessary for the session cookie
+
+all_packages = []
+with open("./staycation.csv", 'r') as f:
+    line = csv.reader(f, delimiter=",")
+    _ = next(line)
+    for row in line:
+        all_packages.append(
+            {
+                "hotel_name": row[0],
+                "duration": row[1],
+                "packageCost": row[2],
+                "image_url": row[3],
+                "description": row[4],
+            }
+        )
+
+def get_package(hotel_name):
+    for package in all_packages:
+        if package["hotel_name"] == hotel_name:
+            break
+    return package
 
 @app.route('/')
 @app.route('/index')
@@ -10,18 +32,21 @@ app.secret_key = "2g3hjkrfhjk34hj"  # necessary for the session cookie
 def home():
     return render_template('login.html', my_title="whatever")
 
-@app.route("/<user_input>")
-def greet(user_input):
-    result = '<p>This is {}.<\p>'.format(user_input)
-    return render_template('base.html', display_string=result)
 
-@app.route('/cards')
-def cards():
-    with open('data.csv', 'r') as f:
-        data = f.read().split('\n')
-    data = [line.split(',') for line in data]
+@app.route('/packages')
+def packages():
+    return render_template('packages.html', data=all_packages)
 
-    return render_template('cards.html', data=data)
+@app.route("/viewPackageDetail/<hotel_name>")
+def viewHotel(hotel_name):
+    package = get_package(hotel_name)
+    return render_template("packages.html", data = [package])
+
+@app.route("/book/<hotel_name>")
+def book(hotel_name):
+    # get the package detail
+    # get check in/out date
+    return render_template("login.html")
 
 @app.route("/login", methods=['POST', 'GET'])
 def form():
